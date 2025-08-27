d90d547b9a53ffe92e25fea58f285beed10d358f
faraday58 <jarodriguezvera@gmail.com>
2025-08-26 20:11:13 -0600

    Add a coment

--- app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
+++ app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
@@ -15,6 +15,10 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,6 +26,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,9 +44,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+//Este es un cambio o comentario
 @Composable
 fun CostGasLayout(name: String) {
+    var precioLitroEntrada by remember{
+        mutableStateOf("")
+    }
+    var cantLitrosEntrada by remember {
+        mutableStateOf("")
+    }
+    var propinaEntrada by remember {
+        mutableStateOf("")
+    }
+
+    val precioLitro = precioLitroEntrada.toDoubleOrNull()  ?: 0.0
+    val cantLitros = cantLitrosEntrada.toDoubleOrNull()  ?: 0.0
+    val propina = propinaEntrada.toDoubleOrNull()  ?: 0.0
+
+    val total = calcularMonto(precioLitro,cantLitros)
+
     Column {
         Text(
             text = stringResource(R.string.calcular_monto),
@@ -54,23 +75,43 @@ fun CostGasLayout(name: String) {
                keyboardType = KeyboardType.Number,
                imeAction = ImeAction.Next
            ),
-           value = "10.0",
-           onValueChanged = {}
+           value = precioLitroEntrada,
+           onValueChanged = {
+               precioLitroEntrada = it
+           }
        )
-        TextField(
-            value = stringResource(R.string.litros),
-            onValueChange = {}
+       EditNumberField(
+           label = R.string.litros,
+           leadingIcon = R.drawable.gasolina,
+           keyboardsOptions = KeyboardOptions.Default.copy(
+               keyboardType = KeyboardType.Number,
+               imeAction = ImeAction.Next
+           ),
+           value = cantLitrosEntrada,
+           onValueChanged = {
+               cantLitrosEntrada = it
+           }
+       )
+        //Componente personalizado
+        EditNumberField(
+            label = R.string.propina,
+            leadingIcon = R.drawable.baseline_monetization_on_24,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = propinaEntrada,
+            onValueChanged = {
+                propinaEntrada = it
+            }
         )
-        TextField(
-            value = stringResource(R.string.propina) ,
-            onValueChange = {}
-        )
+
         Switch(
             checked = false,
             onCheckedChange =  {}
         )
         Text(
-            text = "Total:  $"
+            text = stringResource(R.string.monto_total,total )
         )
 
     }
@@ -104,4 +145,10 @@ fun CostGasLayoutPreview() {
     CostoGasolinaTheme {
         CostGasLayout("Android")
     }
+}
+
+
+private fun calcularMonto(precio:Double, cantLitros: Double):String{
+    val monto = precio * cantLitros
+    return NumberFormat.getCurrencyInstance().format(monto)
 }
